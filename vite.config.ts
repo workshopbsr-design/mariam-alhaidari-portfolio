81c9e7b62e175e1f603f6914eadb3f9d6802e11d
workshopbsr-design <workshop.bsr@gmail.com>
2026-01-23 15:45:39 +0300

    Update vite.config.ts
    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,23 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'url';
-import process from 'process';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  // Use import.meta.url as a type-safe alternative to process.cwd() for determining the current directory
+  const env = loadEnv(mode, fileURLToPath(new URL('.', import.meta.url)), '');
   return {
-    base: '/',
     plugins: [react()],
     resolve: {
-      alias: [{ find: '@', replacement: fileURLToPath(new URL('./', import.meta.url)) }]
+      alias: {
+        '@': fileURLToPath(new URL('./', import.meta.url))
+      }
+    },
+    define: {
+      'process.env': JSON.stringify(env)
     },
     build: {
       outDir: 'dist',
-      sourcemap: false,
-    },
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY),
-      'process.env': JSON.stringify(env)
+      emptyOutDir: true
     }
   };
 });
